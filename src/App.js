17ce5f8b896a698f69cd35df352d188964671e8b
Denis Suhov <suhovdg@MacBook-Air-Denis.local>
2018-12-16 16:59:01 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import styled from 'styled-components';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className="wrapper">
+      <SayFullName name="Денис" surname="Сухов" link="http://vk.com" />
+      <SayFullName name="Александр" surname="Сухов" link="http://facebook.com" />
+      <SayFullName name="Олег" surname="Лучинин" link="http://ok.com" />
+    </div>
+
+      
     );
   }
 }
+
+
+function SayFullName(props){
+  return(
+        <div>
+          <h1>  Мое имя {props.name},  фамилия {props.surname} </h1>
+          <a href={props.link}> Ссылка на мой профиль   </a>
+        </div>
+    )
+}
+
 
 export default App;
